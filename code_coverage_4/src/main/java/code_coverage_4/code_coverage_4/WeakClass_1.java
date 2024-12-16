9aaf766aa48cb8fbbadf9d849c6379cca8fd53f0
chaya7282 <52631833+chaya7282@users.noreply.github.com>
2024-12-16 19:13:52 +0200

    code coverage with jocococ

---
+++ code_coverage_4/src/main/java/code_coverage_4/code_coverage_4/WeakClass_1.java
@@ -0,0 +1,15 @@
+package code_coverage_4.code_coverage_4;
+
+public class WeakClass_1 {
+	
+	public  int weakMethod1(int x, int y) {
+		int c = x / y;
+		if(c > 0)
+			return 1;
+		else
+			return -1;
+		
+	}
+	
+
+}
