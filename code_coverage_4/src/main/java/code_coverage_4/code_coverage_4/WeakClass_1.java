c191770880e93666614e09e3db6f6439cc04dc6b
yazeeddi <yazeed.love.n@gmail.com>
2025-01-09 18:49:35 +0200

    מטלה 4

--- code_coverage_4/src/main/java/code_coverage_4/code_coverage_4/WeakClass_1.java
+++ code_coverage_4/src/main/java/code_coverage_4/code_coverage_4/WeakClass_1.java
@@ -1,15 +1,49 @@
 package code_coverage_4.code_coverage_4;
-
+// yazeed diab 314744228
+// mohamed agbiryh 211405568
 public class WeakClass_1 {
-	
-	public  int weakMethod1(int x, int y) {
-		int c = x / y;
-		if(c > 0)
-			return 1;
-		else
-			return -1;
-		
-	}
-	
 
-}
+    public static class Triangle_classification {
+
+        enum Triangle_Types {
+            equilateral,
+            isosceles,
+            Scalene,
+            Nottriangle,
+            ERROR
+        }
+
+        public static String classifyTriangle(int a, int b, int c) {
+            Triangle_Types triangle;
+
+            // בדיקה אם זה משולש
+            if (c < a + b) {
+                if (b < a + c) {
+                    if (a < b + c) {
+                        // בדיקה אם כל הצלעות שוות
+                        if (a == b) {
+                            if (b == c) {
+                                triangle = Triangle_Types.equilateral;
+                            } else {
+                                triangle = Triangle_Types.isosceles;
+                            }
+                        } else if (b == c) {
+                            triangle = Triangle_Types.isosceles;
+                        } else if (a == c) {
+                            triangle = Triangle_Types.isosceles;
+                        } else {
+                            triangle = Triangle_Types.Scalene;
+                        }
+                    } else {
+                        triangle = Triangle_Types.Nottriangle;
+                    }
+                } else {
+                    triangle = Triangle_Types.Nottriangle;
+                }
+            } else {
+                triangle = Triangle_Types.Nottriangle;
+            }
+
+            return triangle.toString();
+        }
+}}
--- code_coverage_4/src/test/java/code_coverage_4/code_coverage_4/Test_weak_method_1_a.java
+++ code_coverage_4/src/test/java/code_coverage_4/code_coverage_4/Test_weak_method_1_a.java
@@ -5,24 +5,46 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class Test_weak_method_1_a {
-	
-	WeakClass_1 tester= new WeakClass_1();
-	
-	@Test
-	public void weakmethod1b() {
-		int result = tester.weakMethod1(10, 2);
-		int expected = 1;
-		
-		assertEquals(expected, result);
-	}
 
-	@Test
-	public void weakmethod1b2() {
-		int result = tester.weakMethod1(-10, 2);
-		int expected = -1;
-		
-		assertEquals(expected, result);
-	}
+    @Test
+    public void TC_1() {
+        assertEquals("Nottriangle", WeakClass_1.Triangle_classification.classifyTriangle(1, 1, 2));
+    }
 
+    @Test
+    public void TC_2() {
+        assertEquals("Nottriangle", WeakClass_1.Triangle_classification.classifyTriangle(1, 5, 1));
+    }
 
+    @Test
+    public void TC_3() {
+        assertEquals("Nottriangle", WeakClass_1.Triangle_classification.classifyTriangle(5, 1, 1));
+    }
+
+    @Test
+    public void TC_4() {
+        assertEquals("Scalene", WeakClass_1.Triangle_classification.classifyTriangle(2, 3, 4));
+    }
+
+    @Test
+    public void TC_5() {
+        assertEquals("isosceles", WeakClass_1.Triangle_classification.classifyTriangle(2, 2, 3));
+    }
+
+  
+
+   @Test
+    public void TC_6() {
+        assertEquals("isosceles", WeakClass_1.Triangle_classification.classifyTriangle(2, 3, 2));
+    }
+
+    @Test
+   public void TC_7() {
+        assertEquals("isosceles", WeakClass_1.Triangle_classification.classifyTriangle(3, 2, 2));
+    }
+
+    @Test
+    public void TC_8() {
+        assertEquals("equilateral", WeakClass_1.Triangle_classification.classifyTriangle(2, 2, 2));
+    }
 }
